aeef810230a6f2c891160b5171fc4009043eea32
lightish <aksileich@gmail.com>
2019-12-22 18:05:12 +0200

    set up rest framework

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blog_site.urls'
+
+REST_FRAMEWORK = {
+
+}
 
 TEMPLATES = [
     {
