52d3043cbc756896a7911e00e609c2466b989032
lightish <aksileich@gmail.com>
2020-01-08 16:28:51 +0200

    Customized Django Admin page for account

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 
-admin.site.register(Account)
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_joined', 'last_login', 'is_admin', 'is_active', 'is_staff', 'about')
+    search_fields = ('username', 'email', 'about')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    filter_vertical = ()
+    fieldsets = ()
+
+    sortable_by = ('username', 'email', 'date_joined', 'last_login', 'is_admin', 'is_active', 'is_staff')
