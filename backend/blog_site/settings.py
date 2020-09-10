f5c78b5deb16bdb734f914959428a1a78f348be7
Anton Korzh <antonkrzh@outlook.com>
2020-09-10 22:44:51 +0300

    Configure pagination for posts

--- backend/blog_site/settings.py
+++ backend/blog_site/settings.py
@@ -51,9 +51,14 @@ CORS_ALLOWED_ORIGINS = [
 ROOT_URLCONF = 'blog_site.urls'
 
 REST_FRAMEWORK = {
+    # Authentication
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 8,
 }
 
 TEMPLATES = [
